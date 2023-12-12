37e67b7b0ac08d0379db78dc84d4370fda428fd2
YPTTT <hai16112001kk@gmail.com>
2023-12-12 10:16:50 +0700

    .

--- index.js
+++ index.js
@@ -10,26 +10,41 @@ function rotateButton(buttonId) {
     }
 
     rotated[buttonId] = !rotated[buttonId];
+};
+
+window.addEventListener('scroll', function() {
+    var menu = document.getElementById('menu');
+    var footer = document.getElementById('footer');
+
+    var footerOffset = footer.offsetTop;
+    var scrollPosition = window.scrollY + window.innerHeight;
+
+    if (scrollPosition > footerOffset) {
+      menu.style.display = 'none';
+    } else {
+      menu.style.display = 'flex';
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function(){
+var initialProducts = 50000;
+var remainingProducts = 2000; 
+var remainingPercentage = (remainingProducts / initialProducts) * 100;
+
+document.getElementById('remainingSell').style.width = remainingPercentage + '%';
+document.getElementById('remainingCard').style.width = remainingPercentage + '%';
+
+if (remainingPercentage >= 50)
+{
+  document.getElementById('remainingSell').style.backgroundColor= "green";
+} else if (remainingPercentage >= 30)
+{
+  document.getElementById('remainingSell').style.backgroundColor= "yellow";
+} else if (remainingPercentage >= 10)
+{
+  document.getElementById('remainingSell').style.backgroundColor= "orange";
+} else
+{
+  document.getElementById('remainingSell').style.backgroundColor= "red";
 }
-
-// function onClick(event) {
-//     let details = event.target.parentElement;
-//     details.style['overflow'] = 'hidden';
-
-//     if (details.open) {
-//         event.preventDefault();
-//         setTimeout(function () {
-//             details.open = false;
-//             details.style['overflow'] = 'visible';
-//         }, 500);
-//         details.style['max-height'] = '48px';
-//     } else {
-//         details.style['max-height'] = '100vh';
-//     }
-// }
-
-// window.onload = function () {
-//     for (summary of document.getElementsByTagName('summary')) {
-//         summary.addEventListener('click', onClick);
-//     }
-// }
+});
